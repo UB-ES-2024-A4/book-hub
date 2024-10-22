09b0c1d21bef8d7307fffcd322f37586f2cf21a4
vparr <vparri08@gmail.com>
2024-10-22 18:51:01 +0200

    model, crud route(endpoints) de user hechos. Falta comprobar la acceptance criteria a la hora de registrarse(en el route)

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from fastapi import Depends
 from sqlmodel import Session, select
 from app.core.database import get_session
-from app.models.user import User
+from app.models.user import User, UserCreate, UserUpdate
+from app import crud
 
 router = APIRouter()
 
@@ -10,7 +11,7 @@ router = APIRouter()
 # Este es un endpoint dummy, para probar que la API funciona.
 @router.get("/")
 def get_first_user(session: Session = Depends(get_session)):
-    result = session.exec(select(User)).first()
+    result = crud.user.get_user(session=session, user_id=1)
     if result:
         return {"username": result.username}
     return {"error": "No users found"}
@@ -18,44 +19,35 @@ def get_first_user(session: Session = Depends(get_session)):
 # Endpoint para obtener todos los usuarios
 @router.get("/all")
 def get_all_users(session: Session = Depends(get_session)):
-    users = session.exec(select(User)).all()
+    users = crud.user.get_all_users(session=session)
     return users
 
 # Endpoint para crear un usuario
 @router.post("/")
-def create_user(user: User, session: Session = Depends(get_session)):
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
+def create_user(new_user: UserCreate, session: Session = Depends(get_session)):
+    return crud.user.create_user(session=session, user_create=new_user)
 
 # Endpoint para actualizar un usuario
 @router.put("/{user_id}")
-def update_user(user_id: int, user: User, session: Session = Depends(get_session)):
-    db_user : User= session.get(User, user_id)
-    if db_user:
-        db_user.id = user.id
-        db_user.username = user.username
-        session.commit()
-        session.refresh(db_user)
-        return db_user
+def update_user(user_id: int, user: UserUpdate, session: Session = Depends(get_session)):
+    user = crud.user.update_user(session=session, user_id=user_id, user=user)
+    if user:
+        return user
     return {"error": "User not found"}
 
 
 # Endpoint para eliminar un usuario
 @router.delete("/{user_id}")
 def delete_user(user_id: int, session: Session = Depends(get_session)):
-    user = session.get(User, user_id)
+    user = crud.user.delete_user(session=session, user_id=user_id)
     if user:
-        session.delete(user)
-        session.commit()
         return {"message": "User deleted successfully"}
     return {"error": "User not found"}
 
 # Endpoint para obtener un usuario por su ID
 @router.get("/{user_id}")
 def get_user(user_id: int, session: Session = Depends(get_session)):
-    user = session.get(User, user_id)
+    user = crud.user.get_user(session=session, user_id=user_id)
     if user:
         return user
     return {"error": "User not found"}
@@ -63,7 +55,7 @@ def get_user(user_id: int, session: Session = Depends(get_session)):
 # Endpoint para obtener un usuario por su nombre
 @router.get("/name/{name}")
 def get_user_by_name(name: str, session: Session = Depends(get_session)):
-    user = session.exec(select(User).where(User.username == name)).first()
+    user = crud.user.get_user_by_name(session=session, name=name)
     if user:
         return user
     return {"error": "User not found"}
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,7 +8,7 @@ class UserBase(SQLModel):
     username : str = Field(unique=True, index=True)
 
     first_name: str | None = None
-    laset_name: str | None = None
+    last_name: str | None = None
 
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
@@ -16,15 +16,13 @@ class User(UserBase, table=True):
     password : str
 
 class UserCreate(UserBase):
-    email: str 
-    username : str
-
-    first_name: str
-    laset_name: str
+    password: str
 
 
 class UserUpdate(SQLModel):
     username: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
 
 
 class UserOut(UserBase):
