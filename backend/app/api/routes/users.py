d4e337801080937c668c0875579c0f404be62755
gmarvega <mar.vegar1173@gmail.com>
2024-10-23 16:22:47 +0200

    Improved methods location

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -4,6 +4,7 @@ from sqlmodel import Session, select
 from app.core.database import get_session
 from app.models.user import User, UserCreate, UserUpdate
 from app import crud
+from app import utils
 
 router = APIRouter()
 
@@ -25,42 +26,13 @@ def get_all_users(session: Session = Depends(get_session)):
 # Endpoint para crear un usuario
 @router.post("/")
 def create_user(new_user: UserCreate, session: Session = Depends(get_session)):
-    statement = select(User).where(User.email == new_user.email)
-    session_user = session.exec(statement).first()
+    utils.check_existence_email(new_user.email, session)
+    
+    utils.check_existence_usrname(new_user.username, session)
 
-    if session_user:
-        raise HTTPException(
-            status_code=400,
-            detail="This email is already in use.",
-        )
+    utils.check_email_name_length(new_user.username, new_user.first_name, new_user.last_name)
     
-    statement = select(User).where(User.username == new_user.username)
-    session_user = session.exec(statement).first()
-
-    if session_user:
-        raise HTTPException(
-            status_code=400,
-            detail="This username is already in use.",
-        )
-    
-    if len(new_user.username) < 3:
-        raise HTTPException(
-            status_code=400,
-            detail="Username must contain at least 3 characters.",
-        )
-    
-    max_length = 20
-    if (len(new_user.username) > max_length or len(new_user.first_name) > max_length or len(new_user.last_name) > max_length):
-        raise HTTPException(
-            status_code=400,
-            detail="Username, first name and last name must contain at most 20 characters.",
-        )
-    
-    if not (8 <= len(new_user.password) <= 28):
-        raise HTTPException(
-            status_code=400,
-            detail="Password must contain between 8 and 28 characters.",
-        )
+    utils.check_pwd_length(new_user.password)
     
     return crud.user.create_user(session=session, user_create=new_user)
 
@@ -72,32 +44,11 @@ def update_user(user_id: int, user: UserUpdate, session: Session = Depends(get_s
 
     # Check if the username is to be updated
     if session_user.username != user.username:
-        statement = select(User).where(User.username == user.username)
-        in_user = session.exec(statement).first()
-
-        if in_user:
-            raise HTTPException(
-                status_code=400,
-                detail="This username is already in use.",
-            )
+        utils.check_existence_usrname(user.username, session)
     
-        if len(user.username) < 3:
-            raise HTTPException(
-                status_code=400,
-                detail="Username must contain at least 3 characters.",
-            )
+    utils.check_email_name_length(user.username, user.first_name, user.last_name)
     
-    if (len(user.username) > 20 or len(user.first_name) > 20 or len(user.last_name) > 20):
-        raise HTTPException(
-            status_code=400,
-            detail="Username, first name and last name must contain at most 20 characters.",
-        )
-    
-    if not (8 <= len(user.password) <= 28):
-        raise HTTPException(
-            status_code=400,
-            detail="Password must contain between 8 and 28 characters.",
-        )
+    utils.check_pwd_length(user.password)
     
     user = crud.user.update_user(session=session, user_id=user_id, user=user)
     if user:
