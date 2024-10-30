8c2e4bda9a935945dc812ddda95c4fa4a91e59f3
AlejandroDS1 <acantelo7@alumnes.ub.edu>
2024-10-30 15:28:57 +0100

    Fix: Username now works in sign in

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -48,7 +48,7 @@ def get_all_users(*, session: Session) -> Any:
     return users
 
 def get_user_by_email(*, session: Session, email: str) -> User | None:
-    statement = select(User).where(User.email == email)
+    statement = select(User).where(User.email == email or User.username == email)
     session_user = session.exec(statement).first()
     return session_user
 
