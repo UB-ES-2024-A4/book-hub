c5871ddae17821a96a7b2bdebbea447ca703471f
gmarvega <mar.vegar1173@gmail.com>
2024-11-02 14:02:53 +0100

    Added read post method and test

--- backend/app/crud/post.py
+++ backend/app/crud/post.py
@@ -23,3 +23,19 @@ def delete_post(*, session: Session, db_post: Post) -> Post:
     session.delete(db_post)
     session.commit()
     return db_post
+
+def get_post(*, session: Session, post_id: int) -> Any:
+    post = session.get(Post, post_id)
+    return post
+
+def get_all_posts(*, session: Session) -> Any:
+    posts = session.exec(select(Post)).all()
+    return posts
+
+def get_posts_by_book_id(*, session: Session, book_id: int) -> Any:
+    posts = session.exec(select(Post).where(Post.book_id == book_id)).all()
+    return posts
+
+def get_posts_by_user_id(*, session: Session, user_id: int) -> Any:
+    posts = session.exec(select(Post).where(Post.user_id == user_id)).all()
+    return posts
--- backend/tests/crud/test_post.py
+++ backend/tests/crud/test_post.py
@@ -41,4 +41,65 @@ def test_delete_post(db: Session) -> None:
     deleted_post = crud.post.delete_post(session=db, db_post=db_post)
     assert deleted_post
 
+def test_get_post(db: Session) -> None:
+    post_in = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    created_post = crud.post.create_post(session=db, post_create=post_in)
+
+    post = crud.post.get_post(session=db, post_id=created_post.id)
+
+    assert post.book_id == book_id
+    assert post.user_id == user_id
+    assert post.description == description
+    assert post.likes == 0
+
+def test_get_all_posts(db: Session) -> None:
+    post_in1 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in1)
+
+    post_in2 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in2)
+
+    all_posts = crud.post.get_all_posts(session=db)
+
+    assert len(all_posts) > 1
+    for item in all_posts:
+        assert item.book_id
+        assert item.user_id
+        assert item.description
+        assert item.created_at
+        assert item.likes >= 0
+
+def test_get_posts_by_book_id(db: Session) -> None:
+    post_in1 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in1)
+
+    post_in2 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in2)
+
+    all_posts = crud.post.get_posts_by_book_id(session=db, book_id=book_id)
+
+    assert len(all_posts) > 1
+    for item in all_posts:
+        assert item.book_id == book_id
+        assert item.user_id
+        assert item.description
+        assert item.created_at
+        assert item.likes >= 0
+
+def test_get_posts_by_user_id(db: Session) -> None:
+    post_in1 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in1)
+
+    post_in2 = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    crud.post.create_post(session=db, post_create=post_in2)
+
+    all_posts = crud.post.get_posts_by_user_id(session=db, user_id=user_id)
+
+    assert len(all_posts) > 1
+    for item in all_posts:
+        assert item.book_id
+        assert item.user_id == user_id
+        assert item.description
+        assert item.created_at
+        assert item.likes >= 0
 
