6b80e2e9227f703b08dd1048395cecbc04b4a7de
gmarvega <mar.vegar1173@gmail.com>
2024-11-04 18:47:40 +0100

    Added update endpoint tests

--- backend/tests/api/test_posts.py
+++ backend/tests/api/test_posts.py
@@ -93,3 +93,42 @@ def test_create_post(
     assert created_post['data']['description'] == description
     assert created_post['data']['likes'] == 0
 
+def test_update_post_not_found(
+    client: TestClient, db: Session
+) -> None:
+    data = {'description': 'b'}
+
+    r = client.put(
+        f'/posts/-1',
+        json=data
+    )
+
+    updated_post = r.json()
+    assert r.status_code == 404
+    assert updated_post['detail'] == "Post not found."
+
+def test_update_post(
+    client: TestClient, db: Session
+) -> None:
+    user_id, book_id = get_test_parameters(db)
+    new_description = 'b'
+
+    post_in = PostCreate(book_id=book_id, user_id=user_id, description=description, created_at=created_at)
+    created_post = crud.post.create_post(session=db, post_create=post_in)
+
+    data = {'description': new_description}
+    
+    r = client.put(
+        f"/posts/{created_post.id}",
+        json=data    
+    )
+    
+    created_post = r.json()
+
+    assert r.status_code == 200
+    assert created_post['message'] == 'Post updated successfully'
+    assert created_post['data']['description'] == new_description
+
+
+
+
