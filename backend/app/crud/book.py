e5259d76c8378fe4208793d01e69e60809bd7245
gmarvega <mar.vegar1173@gmail.com>
2024-11-08 01:09:44 +0100

    Added Get book method and its test

--- backend/app/crud/book.py
+++ backend/app/crud/book.py
@@ -10,3 +10,19 @@ def create_book(*, session: Session, book_create: BookCreate) -> Book:
     session.commit()
     session.refresh(book)
     return book
+
+def get_book_by_id(*, session: Session, book_id: int) -> Any:
+    book = session.get(Book, book_id)
+    return book
+
+def get_books_by_title(*, session: Session, title: str) -> Any:
+    books = session.exec(select(Book).where(Book.title.contains(title))).all()
+    return books
+
+def get_books_by_author(*, session: Session, author: str) -> Any:
+    books = session.exec(select(Book).where(Book.author.contains(author))).all()
+    return books
+
+def get_all_books(*, session: Session) -> Any:
+    books = session.exec(select(Book)).all()
+    return books
--- backend/tests/crud/test_book.py
+++ backend/tests/crud/test_book.py
@@ -17,3 +17,61 @@ def test_create_book(db: Session) -> None:
     assert created_book.title == title
     assert created_book.author == author
     assert created_book.description == description
+
+def test_get_book_by_id(db: Session) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    created_book = crud.book.create_book(session=db, book_create=book_in)
+
+    book = crud.book.get_book_by_id(session=db, book_id=created_book.id)
+
+    assert book.title == title
+    assert book.author == author
+    assert book.description == description
+
+def test_get_books_by_title(db: Session) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title='BookTest2', author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    all_books = crud.book.get_books_by_title(session=db, title='ook')
+
+    assert len(all_books) > 1
+    for item in all_books:
+        assert 'ook' in item.title.lower()
+        assert item.author
+        assert item.description
+
+def test_get_books_by_author(db: Session) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title=title, author='Second_Tester', description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    all_books = crud.book.get_books_by_author(session=db, author='tes')
+
+    print('AAAAAAAAAAAAA')
+    print(all_books)
+
+    assert len(all_books) > 1
+    for item in all_books:
+        assert item.title
+        assert 'tes' in item.author.lower()
+        assert item.description
+
+def test_get_all_books(db: Session) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    all_books = crud.book.get_all_books(session=db)
+
+    assert len(all_books) > 1
+    for item in all_books:
+        assert item.title
+        assert item.author
+        assert item.description
