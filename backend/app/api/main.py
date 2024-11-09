ee4e44cb07e6aa9204c238643f65a5b7cc0253dc
Mar Vega <113033933+gmarvega@users.noreply.github.com>
2024-11-09 19:11:40 +0100

    Update

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,8 @@
 """ Main API routes definition """
 from fastapi import APIRouter
 
-from app.api.routes import users, posts, books
+from app.api.routes import users, posts
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
-api_router.include_router(books.router, prefix="/books", tags=["books"])
