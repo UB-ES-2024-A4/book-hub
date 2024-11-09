be2a022e847dec38be708021bc40af7e8f18cb60
gmarvega <mar.vegar1173@gmail.com>
2024-11-09 19:22:45 +0100

    Added get endpoint and its tests

--- backend/app/api/routes/books.py
+++ backend/app/api/routes/books.py
@@ -24,3 +24,42 @@ def create_book(new_book: BookCreate, session: Session = Depends(get_session)):
     post = crud.book.create_book(session=session, book_create=new_book)
     
     return {"message": "Book created successfully", "data": post}
+
+# Get all books endpoint
+@router.get("/all")
+def get_all_books(session: Session = Depends(get_session)):
+    books = crud.book.get_all_books(session=session)
+    return books
+
+# Get book by id endpoint
+@router.get("/{book_id}")
+def get_book_by_id(book_id: int, session: Session = Depends(get_session)):
+    book = crud.book.get_book_by_id(session=session, book_id=book_id)
+    if book:
+        return book
+    raise HTTPException(
+        status_code=404,
+        detail="Book not found",
+    )
+
+# Get all books with the same title
+@router.get("/title/{title}")
+def get_books_by_title(title: str = Path(..., min_length=1), session: Session = Depends(get_session)):
+    books = crud.book.get_books_by_title(session=session, title=title)
+    if books:
+        return books
+    raise HTTPException(
+        status_code=404,
+        detail="No books were found",
+    )
+
+# Get all books with the same author
+@router.get("/author/{author}")
+def get_books_by_author(author: str = Path(..., min_length=1), session: Session = Depends(get_session)):
+    books = crud.book.get_books_by_author(session=session, author=author)
+    if books:
+        return books
+    raise HTTPException(
+        status_code=404,
+        detail="No books were found",
+    )
--- backend/tests/api/test_books.py
+++ backend/tests/api/test_books.py
@@ -87,3 +87,138 @@ def test_create_book(
     assert created_book['data']['author'] == author
     assert created_book['data']['title'] == title
     assert created_book['data']['description'] == description
+
+def test_get_all_books(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(author=author, title=title, description=description)
+    crud.book.create_book(session=db, book_create=book_in)
+
+    book_in2 = BookCreate(author=author, title=title, description=description)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    r = client.get(
+        "/books/all"
+    )
+
+    all_books = r.json()
+
+    assert len(all_books) > 1
+    for item in all_books:
+        assert 'title' in item
+        assert 'author' in item
+        assert 'description' in item
+
+def test_get_books_empty(
+    client: TestClient, db: Session
+) -> None:
+    try:
+        db.execute(text('DELETE FROM book'))
+
+        r = client.get(
+            "/books/all"
+        )
+
+        all_books = r.json()
+
+        assert r.status_code == 200
+        assert all_books == []
+    finally:
+        db.rollback()
+
+def test_get_book_by_id(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(author=author, title=title, description=description)
+    book = crud.book.create_book(session=db, book_create=book_in)
+
+    r = client.get(
+        f'/books/{book.id}'
+    )
+
+    assert r.status_code == 200
+    retrieved_book = r.json()
+    assert retrieved_book
+    assert book.author == retrieved_book["author"]
+    assert book.title == retrieved_book["title"]
+    assert book.description == retrieved_book["description"]
+
+def test_get_book_not_found_by_id(
+    client: TestClient, db: Session
+) -> None:
+    r = client.get(
+        f"/books/-1",
+    )
+    
+    assert r.status_code == 404
+    assert r.json()["detail"] == "Book not found"
+
+def test_get_book_by_title_not_found(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title='BookTest2', author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    r = client.get(
+        f'/books/title/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    )
+
+    assert r.status_code == 404
+    assert r.json()["detail"] == "No books were found"
+
+def test_get_book_by_title(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title='BookTest2', author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    r = client.get(
+        f'/books/title/ook'
+    )
+
+    all_books = r.json()
+
+    assert r.status_code == 200
+    for item in all_books:
+        assert 'ook' in item['title'].lower()
+
+def test_get_book_by_author_not_found(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title=title, author='Second_Tester', description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    r = client.get(
+        f'/books/author/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    )
+
+    assert r.status_code == 404
+    assert r.json()["detail"] == "No books were found"
+
+def test_get_book_by_author(
+    client: TestClient, db: Session
+) -> None:
+    book_in = BookCreate(title=title, author=author, description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in)
+    
+    book_in2 = BookCreate(title=title, author='Second_Tester', description=description, created_at=created_at)
+    crud.book.create_book(session=db, book_create=book_in2)
+
+    r = client.get(
+        f'/books/author/tes'
+    )
+
+    all_books = r.json()
+
+    assert r.status_code == 200
+    for item in all_books:
+        assert 'tes' in item['author'].lower()
