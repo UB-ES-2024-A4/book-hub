a67a2ba97b494ff1f7f2b589880fa31d6f777891
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-11-10 12:42:04 +0100

    Account Edit Profile

--- frontend/app/account/components/User/UserProfileData.tsx
+++ frontend/app/account/components/User/UserProfileData.tsx
@@ -1,29 +1,40 @@
 import {Button} from "@/components/ui/button";
 import {Edit2} from "lucide-react";
-import React from "react";
+import React, {useState} from "react";
 import {User} from "@/app/types/User";
+import ProfileDialog from "@/app/account/components/Dialogs/ProfileDialog";
 
-type Props = {
-    userDataMock: User;
-    handleEdit: () => void;
-}
+import {PropsUser} from "@/app/types/PropsUser";
 
-export default function UserProfileData ({userDataMock, handleEdit}: Props) {
+export default function UserProfileData ({userData, setUser}: PropsUser) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
     return (
         <div className="pt-16 px-8 pb-8">
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 items-start">
                 <div className="lg:col-span-3">
-                    <h2 className="text-3xl font-bold">{userDataMock.firstName} {userDataMock.lastName}</h2>
-                    <p className="text-gray-600">@{userDataMock.username}</p>
-                    <p className="mt-2 text-gray-700">{userDataMock.bio}</p>
+                    <h2 className="text-3xl font-bold">{userData.first_name} {userData.last_name}</h2>
+                    <p className="text-gray-600">@{userData.username}</p>
+                    <p className="mt-2 text-gray-700">{userData.biography}</p>
                 </div>
-                {/*<div className="flex flex-col space-y-2 lg:items-end">
+                <div className="flex flex-col space-y-2 lg:items-end">
                     <Button variant="outline" onClick={handleEdit}
                             className="w-full lg:w-auto shadow-sm shadow-black path">
                         <Edit2 className="mr-2 h-4 w-4"/> Edit Profile
                     </Button>
-                </div>*/}
+                </div>
             </div>
+
+      <ProfileDialog
+        isEditing={isEditing}
+        setIsEditing={setIsEditing}
+        userData = {userData}
+        setUser = {setUser}
+      />
         </div>
     )
 }
