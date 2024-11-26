f3cc76ed9b5ed7860ab0fc0bc8c5b7f21202694f
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-11-26 18:18:04 +0100

    User Name Color Home Posts

---
+++ frontend/app/lib/colorHash.tsx
@@ -0,0 +1,8 @@
+export function getColorFromInitials(initials: string): string {
+  let hash = 0;
+  for (let i = 0; i < initials.length; i++) {
+    hash = initials.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const hue = hash % 360;
+  return `hsl(${hue}, 70%, 60%)`;
+}
