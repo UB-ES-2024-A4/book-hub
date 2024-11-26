8ec707d80a4420444e9cd9245adc6c9a826962d1
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-11-26 18:18:42 +0100

    New Setup Backend Updated

--- backend/init_testing_database.sql
+++ backend/init_testing_database.sql
@@ -17,37 +17,43 @@ insert into user values ("malik@test.com",     "Malik",       "Malik",       NUL
 insert into user values ("victor@test.com",    "Victor",      "Victor",      NULL,       5, "$2b$12$r3JW.rmsG11lNEgMu3xYYedyd0sBiMrkXYEy47z8/eH4vnrhYnQfK",  NULL, NULL); # Password is -> "contraseña"
 insert into user values ("arnau@test.com",     "Arnau",       "Arnau",       NULL,       6, "$2b$12$r3JW.rmsG11lNEgMu3xYYedyd0sBiMrkXYEy47z8/eH4vnrhYnQfK",  NULL, NULL); # Password is -> "contraseña"
 
+# Insert books -         ID, TITLE, AUTHOR, DESCRIPTION, CREATED_AT
+insert into book values (1, "Asesinato en el Orient Express", "Agatha Christie", "Un asesinato misterioso ocurre en un lujoso tren, y Hércules Poirot debe resolverlo.", CURRENT_TIMESTAMP);
+insert into book values (2, "Cien años de soledad", "Gabriel García Márquez", "La historia mágica y trágica de la familia Buendía en Macondo.", CURRENT_TIMESTAMP);
+insert into book values (3, "1984", "George Orwell", "Una distopía donde el Gran Hermano lo controla todo, y la libertad es solo un sueño.", CURRENT_TIMESTAMP);
+insert into book values (4, "El principito", "Antoine de Saint-Exupéry", "Un cuento encantador sobre la amistad y el descubrimiento.", CURRENT_TIMESTAMP);
+insert into book values (5, "El señor de los anillos", "J.R.R. Tolkien", "La épica aventura para destruir el Anillo Único y salvar la Tierra Media.", CURRENT_TIMESTAMP);
 
-# Insert Books          ID  TITLE    AUTHOR         DESC   TIME
-insert into book values (1, "1_test", "1_testAuth" , "1_test", CURRENT_TIMESTAMP);
-insert into book values (2, "2_test", "2_testAuth" , "2_test", CURRENT_TIMESTAMP);
-insert into book values (3, "3_test", "3_testAuth" , "3_test", CURRENT_TIMESTAMP);
-insert into book values (4, "4_test", "4_testAuth" , "4_test", CURRENT_TIMESTAMP);
-insert into book values (5, "5_test", "5_testAuth" , "5_test", CURRENT_TIMESTAMP);
+# Insert posts -     ID, BOOKID, USERID, CONTENT, LIKES, CREATED_AT
+insert into post values (2, 1, 2, "Hércules Poirot es el mejor detective. ¡Qué historia! El final me dejó boquiabierta.", 7, CURRENT_TIMESTAMP);
+insert into post values (3, 2, 3, "La narrativa mágica de Macondo me hizo reflexionar sobre la vida y el tiempo.", 10, CURRENT_TIMESTAMP);
+insert into post values (4, 3, 4, "1984 es un libro impactante. Me hizo cuestionar todo sobre el poder y la libertad.", 8, CURRENT_TIMESTAMP);
+insert into post values (5, 5, 5, "Una aventura épica. Tolkien es el maestro de la fantasía.", 12, CURRENT_TIMESTAMP);
 
-# Insert posts          ID BOOK_ID USER_ID, DESC     LIKES  TIME
-insert into post values (1,  1,     1,       "test",   0,    CURRENT_TIMESTAMP);
-insert into post values (2,  1,     1,       "test",   0,    CURRENT_TIMESTAMP);
-insert into post values (3,  2,     2,       "test",   0,    CURRENT_TIMESTAMP);
-insert into post values (4,  2,     3,       "test",   0,    CURRENT_TIMESTAMP);
-insert into post values (5,  3,     4,       "test",   0,    CURRENT_TIMESTAMP);
-
-# Insert filters
+# Insert filters -  ID, NAME
 insert into filter values (1, "Drama");
 insert into filter values (2, "Comedia");
 insert into filter values (3, "Terror");
-insert into filter values (4, "Police");
-insert into filter values (5, "Romance");
-insert into filter values (6, "Horror");
+insert into filter values (4, "Fantasía");
+insert into filter values (5, "Aventura");
+insert into filter values (6, "Distopía");
+insert into filter values (7, "Ciencia Ficción");
+insert into filter values (8, "Romance");
+insert into filter values (9, "Police");
 
-# Insert postFilters         postID, FILTERID
-insert into postfilter values(1, 2);
-insert into postfilter values(1, 3);
+# Insert postFilters linking posts to filters
+insert into postfilter values (2, 3); -- Asesinato en el Orient Express: misterio, drama
+insert into postfilter values (2, 1);
+insert into postfilter values (2, 9);
 
-insert into postfilter values(2, 1);
-insert into postfilter values(2, 2);
-insert into postfilter values(2, 5);
+insert into postfilter values (3, 1); -- Cien años de soledad: drama mágico
+insert into postfilter values (3, 4);
 
+insert into postfilter values (4, 6); -- 1984: distopía, drama
+insert into postfilter values (4, 1);
+
+insert into postfilter values (5, 4); -- El señor de los anillos: fantasía, aventura
+insert into postfilter values (5, 5);
 
 # Insert Likes            ID    USRID POSTID
 insert into `like` values ( 1, 1);
@@ -58,13 +64,13 @@ insert into `like` values ( 2, 2);
 insert into `like` values ( 1, 2);
 insert into `like` values ( 1, 3);
 
-# Insert Comments           ID      USER_ID   POST_ID  COMMENT   CREATED_AT
-insert into comment values (NULL,  1,        1,      "Test Comment User 1", CURRENT_TIMESTAMP);
-insert into comment values (NULL,  1,        2,      "Test Comment User 1", CURRENT_TIMESTAMP);
-insert into comment values (NULL,  1,        1,      "Test Comment user 2", CURRENT_TIMESTAMP);
-insert into comment values (NULL,  2,        1,      "Test Comment User 2", CURRENT_TIMESTAMP);
-insert into comment values (NULL,  2,        3,      "Test Comment User 2", CURRENT_TIMESTAMP);
-insert into comment values (NULL,  3,        3,      "Test Comment User 3", CURRENT_TIMESTAMP);
+# Insert Comments (with relevant comments)
+insert into comment values (NULL, 1, 1, "¡Me encantó el final! Agatha Christie es increíble.", CURRENT_TIMESTAMP);
+insert into comment values (NULL, 2, 1, "Poirot es un genio, este caso fue alucinante.", CURRENT_TIMESTAMP);
+insert into comment values (NULL, 3, 3, "La historia de los Buendía es tan compleja como hermosa.", CURRENT_TIMESTAMP);
+insert into comment values (NULL, 4, 4, "1984 me dejó pensando mucho en la privacidad hoy en día.", CURRENT_TIMESTAMP);
+insert into comment values (NULL, 5, 5, "La Tierra Media es un mundo espectacular. Recomendado.", CURRENT_TIMESTAMP);
+
 
 # Make users follow each other
 insert into followers values (1, 2);
@@ -76,8 +82,8 @@ insert into followers values (3, 1);
 insert into followers values (3, 2);
 insert into followers values (4, 5);
 insert into followers values (5, 4);
-# Show all database
 
+# Show all database content
 select * from user;
 select * from post;
 select * from book;
