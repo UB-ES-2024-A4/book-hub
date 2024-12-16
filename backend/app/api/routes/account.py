24a6f8af830aa987497eafebc73f3de909fc2990
AlejandroDS1 <acantelo7@alumnes.ub.edu>
2024-12-16 21:59:21 +0100

    Added Error correction to account method

--- backend/app/api/routes/account.py
+++ backend/app/api/routes/account.py
@@ -12,4 +12,7 @@ router = APIRouter()
             dependencies=[Depends(get_current_user), Depends(get_session)])
 def get_account(user_id : int, session: Session = Depends(get_session), user : User = Depends(get_current_user)):
     
-    return get_account_posts(session=session, user_id=user.id, user_id_acc=user_id)
+    try:
+        return get_account_posts(session=session, user_id=user.id, user_id_acc=user_id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="An error occourred while retrieving the account data")
