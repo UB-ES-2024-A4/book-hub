61f7ad78998ac57658de0a582040d8c9422a8073
AlejandroDS1 <acantelo7@alumnes.ub.edu>
2024-12-16 01:20:56 +0100

    Profile Picture size optimization implemented

--- frontend/app/account/components/ProfileHeader.tsx
+++ frontend/app/account/components/ProfileHeader.tsx
@@ -5,6 +5,7 @@ import { PropsUser } from "@/app/types/PropsUser";
 import {putProfilePictureBackend} from "@/app/actions";
 import UserProfilePicture from "@/app/account/components/User/UserProfilePicture";
 import UserProfileData from "@/app/account/components/User/UserProfileData";
+import imageCompression from "browser-image-compression";
 
 import "nextjs-toast-notify/dist/nextjs-toast-notify.css";
 export default function ProfileHeader({ userData, setUser }: PropsUser) {
@@ -13,11 +14,28 @@ export default function ProfileHeader({ userData, setUser }: PropsUser) {
   const handleProfilePictureChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      console.log('Changing profile picture:', file);
+     
+      const options = {
+        maxSizeMB: 0.001, // Tamaño máximo de 1 KB (1 / 1024 MB)
+        maxWidthOrHeight: 200, // Escala máxima en píxeles
+        useWebWorker: true, // Usa Web Worker para optimizar
+        initialQuality: 0.1, // Empieza con una calidad baja
+      };
+
+      console.log("Compressing iimage");
+
+
+      let compressedImage: File | undefined;
+      if (file && file instanceof File) {
+          compressedImage = await imageCompression(file, options);
+      } else {
+          throw new Error("Invalid file");
+      }
+      
+      console.log("Image compressed");
 
       const formData = new FormData();
-      formData.append('file', file);
-
+      formData.append('file', compressedImage);
       // Upload the file to the backend
       await putProfilePictureBackend(formData, userData.id);
     }
