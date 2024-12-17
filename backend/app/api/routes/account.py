dc671111486183b41698014d2687b51720bb2a14
AlejandroDS1 <acantelo7@alumnes.ub.edu>
2024-12-17 03:21:29 +0100

    feat: Endpoint retrieve post likes

--- backend/app/api/routes/account.py
+++ backend/app/api/routes/account.py
@@ -3,7 +3,7 @@ from sqlmodel import Session
 from app.models.account_info import PostRepository
 from app.core.database import get_session
 from app.models.user import User
-from app.api.deps import (get_optional_user)
+from app.api.deps import (get_optional_user, get_current_user)
 
 router = APIRouter()
 
@@ -18,3 +18,12 @@ def get_account(user_id : int, session: Session = Depends(get_session), user : U
         return PostRepository(session).get_user_posts_with_comments(user.id if user else None, user_id)
     except Exception:
         raise HTTPException(status_code=500, detail="An error occurred while retrieving posts")
+    
+@router.get("/liked/",
+            dependencies=[Depends(get_current_user), Depends(get_session)])
+def get_posts_liked(session: Session = Depends(get_session), user : User = Depends(get_current_user)):
+
+    # try:
+    return PostRepository(session).get_liked_posts_by_user(user.id)
+    # except Exception:
+    #     raise HTTPException(status_code=500, detail="An error occurred while retrieving posts")
